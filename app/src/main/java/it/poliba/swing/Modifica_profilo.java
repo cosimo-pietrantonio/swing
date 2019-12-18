235c51bc5d52f6405c9613430ee2cc04bc182025
nicola <scaranonicola2503@gmail.com>
2019-12-18 17:11:52 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/it/poliba/swing/Register_Activity.java

---
+++ app/src/main/java/it/poliba/swing/Modifica_profilo.java
@@ -0,0 +1,154 @@
+package it.poliba.swing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.SyncConfiguration;
+import io.realm.SyncUser;
+
+public class Modifica_profilo extends AppCompatActivity implements DatePickerDialog.OnDateSetListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifica_profilo);
+
+        final EditText nome = findViewById(R.id.no);
+        final EditText cognome = findViewById(R.id.co);
+        final TextView email = findViewById(R.id.em);
+        final EditText data = findViewById(R.id.da);
+        final EditText password = findViewById(R.id.pa);
+        final int StartYear = 2000;
+        final int StartMonth= 12;
+        final int StartDay= 31;
+        Button salva = findViewById(R.id.salva);
+        utente u = new utente();
+
+
+        //data picker dialog
+        final DatePickerDialog datePickerDialog = new DatePickerDialog(this,Modifica_profilo.this,StartYear,StartMonth,StartDay );
+
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                //DatePicker date = datePickerDialog.getDatePicker();
+
+                String day,month,year;
+                day= Integer.toString(i2);
+                month=Integer.toString(i1+1);
+                year=Integer.toString(i);
+                String data1=day+"/"+month+"/"+year;
+                data.setText(data1);
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Email non modificabile", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // cofigurazione del DB nell'activity
+        String syncServerURL = "https://swing-app.de1a.cloud.realm.io/temp2";
+        final SyncConfiguration config = new SyncConfiguration.Builder(SyncUser.current(), syncServerURL).build();
+        final Realm realm = Realm.getInstance(config);
+
+        //ricezione dati utete loggato
+        Intent b = getIntent();
+        Bundle b1 = b.getExtras();
+        if (b1 != null) {
+            u = b1.getParcelable("object_key");
+        }
+
+        nome.setHint(u.getNome());
+        cognome.setHint(u.getCognome());
+        email.setText(u.getEmail());
+        data.setHint(u.getDataNascita());
+        password.setHint(u.getPassword());
+
+        final utente finalU = new utente();
+        final utente finaluu = new utente();
+        finaluu.setPassword(u.getPassword());
+        salva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finalU.setCognome(cognome.getText().toString());
+                finalU.setNome(nome.getText().toString());
+                finalU.setDataNascita(data.getText().toString());
+                finalU.setEmail(email.getText().toString());
+                finalU.setPassword(password.getText().toString());
+                final String email = finalU.getEmail();
+
+                //controllo
+                final String Cognome;
+                if(cognome.getText().toString().length()!=0){
+                    Cognome =finalU.getCognome();
+                }else{
+                    Cognome= cognome.getHint().toString();
+                }
+
+                final String Nome;
+                if(nome.getText().toString().length()!=0){
+                    Nome =finalU.getNome();
+                }else{
+                    Nome= nome.getHint().toString();
+                }
+
+                final String Data;
+                if(data.getText().toString().length()!=0){
+                    Data =finalU.getDataNascita();
+                }else{
+                    Data= data.getHint().toString();
+                }
+
+                final String Password;
+                if(password.getText().toString().length()!=0){
+                    Password =finalU.getPassword();
+                }else{
+                    Password= password.getHint().toString();
+                }
+
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        RealmResults <utente> utenti = realm.where(utente.class).equalTo("email",email).findAll();
+                        utenti.setValue("nome",Nome );
+                        utenti.setValue("cognome", Cognome);
+                        utenti.setValue("password", Password);
+                        utenti.setValue("dataNascita", Data);
+                        Toast.makeText(getApplicationContext(), "Modifica effettuata", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+
+                });
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+    }
+
+}
---
+++ app/src/main/java/it/poliba/swing/Profilo.java
@@ -0,0 +1,116 @@
+package it.poliba.swing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+import io.realm.SyncConfiguration;
+import io.realm.SyncUser;
+
+public class Profilo extends AppCompatActivity implements io.realm.RealmModel{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilo);
+
+        // cofigurazione del DB nell'activity
+        String syncServerURL = "https://swing-app.de1a.cloud.realm.io/temp2";
+        final SyncConfiguration config = new SyncConfiguration.Builder(SyncUser.current(), syncServerURL).build();
+        final Realm realm = Realm.getInstance(config);
+
+        final Intent a = new Intent(this, Modifica_profilo.class);
+        utente utente = new utente();
+        final utente U =new utente();
+        TextView e = findViewById(R.id.em);
+        TextView c = findViewById(R.id.co);
+        TextView n = findViewById(R.id.no);
+        TextView d = findViewById(R.id.da);
+        TextView pa = findViewById(R.id.pa);
+        final LinearLayout lin= findViewById(R.id.attivi);
+        Button add = findViewById(R.id.add);
+        Button modifica = findViewById(R.id.modifica);
+        findViewById(R.id.addd).setSelected(true);
+
+
+        //ricezione dati utete loggato
+        Intent b = getIntent();
+        Bundle b1 = b.getExtras();
+        if (b1 != null) {
+            utente = b1.getParcelable("object_key");
+        }
+
+
+        //lettura richieste e offerte ad esso associato
+        RealmQuery<Richiesta> query1 = realm.where(Richiesta.class);
+        RealmQuery<Offerta> query2 = realm.where(Offerta.class);
+        //query1.equalTo("email", utente.getEmail());
+        //query2.equalTo("email", utente.getEmail());
+        final RealmResults<Richiesta> result1 = query1.findAll();
+        final RealmResults<Offerta> result2 = query2.findAll();
+
+
+        e.setText(utente.getEmail());
+        c.setText(utente.getCognome());
+        n.setText(utente.getNome());
+        d.setText(utente.getDataNascita());
+        pa.setText("Password: "+utente.getPassword());
+
+        U.setEmail(utente.getEmail());
+        U.setDataNascita(utente.getDataNascita());
+        U.setNome(utente.getNome());
+        U.setCognome(utente.getCognome());
+        U.setPassword(utente.getPassword());
+
+
+        modifica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle b1 = new Bundle();
+                b1.putParcelable("object_key", U);
+                a.putExtras(b1);
+                startActivity(a);
+                finish();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TextView add = new TextView(Profilo.this);
+                //get partenza destinazione data
+                add.setText("Bari-noci   ore: 15:00  ciao ciao");
+                add.setPadding(5,5,0,15);
+                add.setTextColor(Color.BLACK);
+                add.setTextSize(25);
+                add.setBackgroundColor(Color.WHITE);
+                add.setFocusable(true);
+                add.setSingleLine(true);
+                add.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+                add.setMarqueeRepeatLimit(100);
+                add.setFocusable(true);
+                add.setFocusableInTouchMode(true);
+                add.setHorizontallyScrolling(true);
+                add.setSelected(true);
+                lin.addView(add);
+            }
+        });
+
+        /*// cofigurazione del DB nell'activity
+        String syncServerURL = "https://swing-app.de1a.cloud.realm.io/swingdb";
+        final SyncConfiguration config = new SyncConfiguration.Builder(SyncUser.current(), syncServerURL).build();
+        final Realm realm = Realm.getInstance(config);*/
+    }
+}
